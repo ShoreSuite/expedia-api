401176748a73d43358f5b2e23ac2d5c26a38b9cf
Jules <julio.telan@shoresuite.com>
2017-10-09 14:51:17 +0800

    Initial Commit

---
+++ lib/expedia/api/ar.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'active_support/concern'
+require 'xml_mapper'
+require 'nokogiri'
+require 'expedia/resource'
+
+module Expedia
+  module API
+    # Expedia QuickConnect Avail & Rates API
+    # https://expediaconnectivity.com/apis/availability-rates-restrictions-booking-notification-retrieval-and-confirmation/expedia-quickconnect-avail-rates-api/quick-start.html
+    module AR
+      # rubocop:disable Metrics/AbcSize
+      def post_ar(property_id, options = {})
+        # rubocop:enable Metrics/AbcSize
+        conn = make_conn
+        resp = conn.post '/eqc/ar' do |req|
+          req.headers['Content-Type'] = 'text/xml'
+          builder = Nokogiri::XML::Builder.new do
+            AvailRateUpdateRQ xmlns: 'http://www.expediaconnect.com/EQC/AR/2011/06' do
+              Authentication username: username, password: password
+              Hotel id: property_id
+              AvailRateUpdate do
+                DateRange from: options[:start_date], to: options[:end_date]
+                options[:room_types].each do |room_type|
+                  RoomType id:room_type[:id], closed: room_type[:closed] do
+                    Inventory totalInventoryAvailable: room_type[:inventory] unless room_type[:inventory]
+                  end
+                end
+              end
+            end
+          end
+          puts builder.to_xml if ENV.key?('EQC_DEBUG_LOG') && ENV['EQC_DEBUG_LOG']
+          req.body = builder.to_xml
+        end
+        doc = Nokogiri::XML(resp.body)
+        if doc.at('Success')
+          200
+        else
+          400
+        end
+      end
+    end
+  end
+end
---
+++ spec/ar_api_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'expedia/api/client'
+require 'expedia/api/ar'
+
+# rubocop:disable Metrics/BlockLength
+# rubocop:disable Style/NumericLiterals
+RSpec.describe Expedia::API::Client, :vcr do
+  describe 'post_ar' do
+    it 'should update availability' do
+      client = Expedia::API::Client.new username: 'EQC16636797hotel', password: 'Te$ting123'
+      property_id = client.list_properties.first.resource_id
+      room_types = client.list_room_types(property_id).map do |room_type|
+        {
+          id: room_type.resource_id,
+          closed: false,
+          inventory: 10
+        }
+      end
+      options = {
+        start_date: '2017-11-01',
+        end_date: '2017-11-15',
+        room_types: room_types
+      }
+      response_status = client.post_ar(property_id, options)
+      expect(response_status).to eq(200)
+    end
+  end
+end
