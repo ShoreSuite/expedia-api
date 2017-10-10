223fc845a8ad6c8f635a9419ec0d350efd508d74
Jules <julio.telan@shoresuite.com>
2017-10-10 11:42:40 +0800

    Final commit for AR

--- lib/expedia/api/ar.rb
+++ lib/expedia/api/ar.rb
@@ -12,15 +12,19 @@ module Expedia
     module AR
       # rubocop:disable Metrics/AbcSize
       # rubocop:disable Metrics/MethodLength
+      # rubocop:disable Metrics/CyclomaticComplexity
+      # rubocop:disable Metrics/PerceivedComplexity
       def post_ar(property_id, options = {})
         # rubocop:enable Metrics/AbcSize
         # rubocop:enable Metrics/MethodLength
+        # rubocop:enable Metrics/CyclomaticComplexity
+        # rubocop:enable Metrics/PerceivedComplexity
         conn = make_conn
         # rubocop:disable Metrics/BlockLength
         resp = conn.post '/eqc/ar' do |req|
-          # rubocop:enable Metrics/BlockLength
           req.headers['Content-Type'] = 'text/xml'
           builder = Nokogiri::XML::Builder.new do
+            # rubocop:enable Metrics/BlockLength
             AvailRateUpdateRQ xmlns: 'http://www.expediaconnect.com/EQC/AR/2011/06' do
               Authentication username: username, password: password
               Hotel id: property_id
@@ -31,13 +35,16 @@ module Expedia
                     Inventory totalInventoryAvailable: room_type[:inventory] if room_type[:inventory]
                     room_type[:rate_plans]&.each do |rate_plan|
                       RatePlan id: rate_plan[:id], closed: rate_plan[:closed] do
-                        Rate currency: rate_plan[:currency] do
-                          PerDay rate: rate_plan[:rate]
+                        if rate_plan[:rate]
+                          Rate currency: rate_plan[:currency] do
+                            PerDay rate: rate_plan[:rate]
+                          end
                         end
                         if rate_plan[:restrictions]
                           restrictions = rate_plan[:restrictions]
-                          Restrictions minLos: restrictions[:minLos], closedToArrival: restrictions[:closedToArrival],
-                                       closedToDeparture: restrictions[:closedToDeparture]
+                          Restrictions minLOS: restrictions[:minLOS], closedToArrival: restrictions[:closedToArrival],
+                                       closedToDeparture: restrictions[:closedToDeparture],
+                                       maxLOS: restrictions[:maxLOS]
                         end
                       end
                     end
@@ -50,6 +57,7 @@ module Expedia
           req.body = builder.to_xml
         end
         doc = Nokogiri::XML(resp.body)
+        puts "DOC => #{doc.to_xml.inspect}"
         if doc.at('Success')
           200
         else
